7a39c81f014a3758f2b4b9901e31c60c218ead7a
newbird <newbird@newbird.hot>
2025-05-07 17:03:30 +0300

    KS-1-2 create

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,16 @@
+package lesson_1
+
+fun main() {
+    val numberOfOrders: Int = 75
+    val gratitudeMassage: String = "Спасибо за покупку!"
+
+    println(numberOfOrders)
+    println(gratitudeMassage)
+
+    var numbersOfEmployees: Int = 2000
+
+    //println(numbersOfEmployees)
+
+    numbersOfEmployees = 1999
+    println(numbersOfEmployees)
+}
