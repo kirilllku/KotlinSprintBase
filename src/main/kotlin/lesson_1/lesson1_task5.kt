834e09441bec659d67e9845507bc3459865fc42a
newbird <newbird@newbird.hot>
2025-05-09 02:19:34 +0300

    KS-1-5 fix magic number, empty string

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,18 +1,17 @@
 package lesson_1
 
-fun main() {
+const val secondsPerMinute = 60
+const val minutesPerHour = 60
 
+fun main() {
     val seconds = 6480
 
-    val hours = seconds / 60 / 60
-    var remainigSeconds = seconds - hours * 60 * 60
+    val hours = seconds / minutesPerHour / secondsPerMinute
+    var remainigSeconds = seconds - hours * minutesPerHour * secondsPerMinute
 
-    val minutes = remainigSeconds / 60
-    remainigSeconds = remainigSeconds - minutes * 60
+    val minutes = remainigSeconds / minutesPerHour
+    remainigSeconds = remainigSeconds - minutes * secondsPerMinute
 
     val Time = String.format("%02d:%02d:%02d", hours, minutes, remainigSeconds)
     println(Time)
-
-
-
 }
