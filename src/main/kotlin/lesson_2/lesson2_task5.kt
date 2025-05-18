688e1e6cc3e5d1b25eac4eacceae2d4aaa0219c4
newbird <newbird@newbird.hot>
2025-05-18 03:03:09 +0300

    KS-2-5 fix initialize variable with percentage

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -1,22 +1,12 @@
 package lessons_2
-import kotlin.math.*
+import kotlin.math.pow
 
 fun main() {
     val initialBid = 70_000
-    //val percent = 16.7f
-    //val percent = 0.167f
-    val percent = 0.167
+    val percent = 16.7
     val time = 20
 
-    //val sumOfBid = initialBid * ((1 + percent * 0.01).pow(time))
-    // percent = 16.7 ANSWER: 1536438,629
-
-    //val sumOfBid = initialBid * ((1 + percent).pow(time))
-    // percent = 0.167f ANSWER: 1536438,875
-
-    val sumOfBid = initialBid * ((1 + percent).pow(time))
-    //ANSWER: 1536438,428
+    val sumOfBid = initialBid * ((1 + percent / 100).pow(time))
 
     println("%.3f".format(sumOfBid))
-
 }
